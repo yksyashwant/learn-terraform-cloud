49f69dc59daa01ca723f80930d9ad3e3a1cf14f9
Apple <apple@Apples-MacBook-Air.local>
2023-02-04 18:00:42 +0530

    removed cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "yksyashwant"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
